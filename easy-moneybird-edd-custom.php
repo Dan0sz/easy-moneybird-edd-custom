662f82b7b3e9e4eb4039d9ec935ce3bd94455073
Dan0sz <18595395+Dan0sz@users.noreply.github.com>
2023-09-22 14:59:57 +0200

    Added: don't sync payment, when Mollie PayPal is used.

---
+++ easy-moneybird-edd-custom.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Plugin Name: Moneybird for Easy Digital Downloads (Customizations)
+ * Plugin URI: https://daan.dev/wordpress/easy-moneybird-edd/
+ * Description: Customizations for Moneybird for EDD.
+ * Version: 1.0.0
+ * Author: Daan from Daan.dev
+ * Author URI: https://daan.dev/about/
+ * Text Domain: easy-moneybird-edd
+ */
+
+defined( 'ABSPATH' ) || exit;
+
+class EDDMoneybirdCustomizations {
+	public function __construct() {
+		add_filter( 'edd_moneybird_purchase_sync_payment', [ $this, 'maybe_sync_payment' ], 9, 3 );
+	}
+	
+	/**
+	 * Make sure we don't sync PayPal payments.
+	 *
+	 * @param bool $value Return value
+	 * @param array $invoice Invoice data from Moneybird API.
+	 * @param EDD_Payment $payment
+	 *
+	 * @return bool
+	 */
+	public function maybe_sync_payment( $value, $invoice, $payment ) {
+		$gateway = $payment->gateway ?? '';
+		
+		if ( ! $gateway ) {
+			return $value;
+		}
+		
+		return $gateway !== 'mollie_paypal';
+	}
+}
+
+new EDDMoneybirdCustomizations();
